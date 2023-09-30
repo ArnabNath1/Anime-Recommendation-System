a4da08af7b71d8a4cca37c580d6850b649d72ed3
Arnab Nath <arnabbumba077@gmail.com>
2023-09-30 16:57:05 +0530

    Added files

--- app.py
+++ app.py
@@ -1,87 +1,55 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import streamlit as st
-import joblib
 import pandas as pd
-from PIL import Image
+import streamlit as st
+import pickle
+import requests
 
 
-# In[2]:
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=f6028648cc42927cbf2f2936bc2d99e2&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
-#@st.cache(allow_output_mutation=True)
-@st.cache_data
-def load(model_path):
-    df = pd.read_pickle(model_path)
-    return df
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for x in movies_list:
+        movie_id = movies.iloc[x[0]].movie_id
+        recommended_movies.append(movies.iloc[x[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
 
 
-# In[4]:
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+st.title('Movie Recommender')
 
-loaded_model=joblib.load('similarities.joblib')
+selected_movie_name = st.selectbox(
+    'How would you like to be contacted?',
+    movies['title'].values
+)
 
-
-# In[5]:
-
-
-new_df=pd.read_csv('data.csv')
-
-
-# In[7]:
-
-list_1=[]
-def recommend(anime):
-    movie_index = new_df[new_df['Name'] == anime].index[0]
-    distances = loaded_model[movie_index]
-    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:15]
-    for i in movies_list:
-        list_1.append(new_df.iloc[i[0]].Name)    
-    df_result=pd.DataFrame(list_1)
-    return df_result
-
-
-# In[8]:
-
-
-#recommend("Shingeki no Kyojin")
-
-
-# In[9]:
-
-
-st.title('Anime Recommendation App')
-st.write('Based on your favs we will give you another 10 anime to binge watch')
-image = Image.open('image.png')
-
-
-# In[10]:
-
-
-st.image(image, use_column_width=True)
-dataframe = load('models/df.zip')
-
-
-# In[11]:
-
-
-option = st.selectbox('Please select your favorite anime', (dataframe.columns))
-
-st.write('You selected:', option)
-
-if (st.button('Get Recommendation')):
-    # dataframe = load('../models/df.pkl')
-    result = recommend(option)
-    st.write(result.head(10))
-    st.balloons()
-
-
-# In[ ]:
-
-
-
-
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
